7ab3a5fd6546b270b0aa2d6a0085a1c6ed08dcae
kitaura <s.kitaura@core-tech.jp>
2025-06-23 10:50:22 +0900

    api.phpに認証、ログイン、ログアウトのパスを追加

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//認証、ログイン、ログアウトのためのコントローラーを追加
+use App\Http\Controllers\AuthController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//ユーザデータの登録、ログイン
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//認証完了している場合のみ実行
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
