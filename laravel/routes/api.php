ed1aa4c8caa1465e914f9bd137ac00a85498d19f
kitaura <s.kitaura@core-tech.jp>
2025-06-23 13:45:05 +0900

    middleware内にlogoutを格納

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 //認証完了している場合のみ実行
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
