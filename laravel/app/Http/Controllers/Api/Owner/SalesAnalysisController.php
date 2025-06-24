39fb1e948a375a6f6c009097633168033e75d493
shindo mitsuki <m.shindo@core-tech.jp>
2025-06-24 15:56:00 +0900

    進藤が売上分析機能のコントローラを基準

--- laravel/app/Http/Controllers/Api/Owner/SalesAnalysisController.php
+++ laravel/app/Http/Controllers/Api/Owner/SalesAnalysisController.php
@@ -5,15 +5,39 @@ namespace App\Http\Controllers\Api\Owner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Sale_Item;//追加
 
 class SalesAnalysisController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        //クエリパラメータ：月か一日か
+        $type  = $request->query('type','monthly');//デフォルトはmonthly
+        //フォーマットを選択：
+        $format = $type === 'dally'? '%Y-%m-%d' :'&Y-%m';
+
+        //売上データを日別か月別で集計
+        $salasData = Sale_Item::select(
+            //SQLの関数を直接使う（SUM()やDATE_FORMAT()
+            DB::raw("DATE_FORMAT(sales.created.at,'$format') as date"),
+            DB::raw("SUM(purchase_amount) as total_sales_amount"),
+            DB::raw("SUM(purchase_num) as total_item_sold")
+        )
+        //sales_items と sales を結合（purchase日時を使うため）
+        ->join('sales','sales_items.sale_id','=','sales.id')
+        //指定した日や月でまとめる
+        ->groupBy('date')
+        // 新しい日付が上に来るように並べる
+        ->orderBy('date','desc')
+        ->get();
+
+        return response()->json([
+            'data' => $salasData
+        ]);
     }
 
     /**
