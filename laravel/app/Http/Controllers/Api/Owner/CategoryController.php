7a0145398dfcc4b607d0c1d8086320d5550a60e8
shindo mitsuki <m.shindo@core-tech.jp>
2025-06-24 13:08:03 +0900

    進藤がカテゴリーコントローラを実装

--- laravel/app/Http/Controllers/Api/Owner/CategoryController.php
+++ laravel/app/Http/Controllers/Api/Owner/CategoryController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Api\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Psy\Util\Json;
+
+use App\Http\Requests\CategoryRequest;//リクエスト追加
+use  App\Http\Resources\owner\CategoryListResource;//リソース追加
 
 class CategoryController extends Controller
 {
@@ -12,15 +19,24 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::orderBy('sort_order')
+            ->get();//ソート優先度を昇順で並びかえて表示
+
+        return CategoryListResource::collection($categories)
+            ->json(['data' => $categories], 200);
+        // 取得したデータをCategoryListResourceに変換し、統一フォーマットで返却
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
+        $category = Category::create($request->validate());
+        // 登録完了メッセージを追加してレスポンス
+        return (new CategoryListResource($category))
+            ->additional(['message' => 'カテゴリーが登録されました'])->response()
+            ->setStatusCode(201);
     }
 
     /**
@@ -28,15 +44,21 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return new CategoryListResource($category);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CategoryRequest $request, string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->update($request->validated());
+        return response()->json([
+            'message' => '更新成功',
+            'data' => $category
+        ]);
     }
 
     /**
@@ -44,6 +66,8 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json(['message' => 'カテゴリを削除しました'], 200);
     }
 }
