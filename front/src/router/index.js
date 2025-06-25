fc141466af761bfa0982906e43169eba34547024
shindo mitsuki <m.shindo@core-tech.jp>
2025-06-25 16:34:27 +0900

    index.jsを変更。「/」で「/login」にリダイレクトするようにしました。

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 import Login from '../views/user/LoginView.vue'
 import Logout from '../views/user/LogoutView.vue'
@@ -67,71 +67,72 @@ import UserFavoritesIndex from '../views/toyzamas/favorites/FavoritesIndex.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView, },
-    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue'),},
+    // { path: '/', name: 'home', component: HomeView, },
+    {path: '/',redirect: '/login'},
+    // { path: '/about', name: 'about', component: () => import('../views/AboutView.vue'),},
 
     //userディレクトリのルート設定
-    { path: '/login', name: 'login', component: Login }, //ログイン用ルートの追加
+    { path: '/login', name: 'login', component: Login,  meta: { layout: 'user' }, }, //デフォルトはuserにしてます。//ログイン用ルートの追加
 
-    { path: '/logout', name: 'logout', component: Logout }, //ログアウト用ルートの追加
+    { path: '/logout', name: 'logout', component: Logout,  meta: { layout: 'user' }, }, //ログアウト用ルートの追加
     
-    { path: '/register', name: 'register', component: Register }, //登録用ルートの追加
+    { path: '/register', name: 'register', component: Register,  meta: { layout: 'user' }, }, //登録用ルートの追加
 
     //Ownerディレクトリのルート設定
-    { path: '/owner', name: 'OwnerTop', component: OwnerTop },
+    { path: '/owner', name: 'OwnerTop', component: OwnerTop,  meta: { layout: 'owner' },},
 
     // 管理者:商品画面ルート
-    { path: '/owner/toys', name: 'OwnerToysIndex', component: OwnerToysIndex },
-    { path: '/owner/toys/create', name: 'OwnerToysCreate', component: OwnerToysCreate },
-    { path: '/owner/toys/:toy', name: 'OwnerToysShow', component: OwnerToysShow, props: true },
-    { path: '/owner/toys/:toy/edit', name: 'OwnerToysEdit', component: OwnerToysEdit, props: true },
+    { path: '/owner/toys', name: 'OwnerToysIndex', component: OwnerToysIndex,  meta: { layout: 'owner' }, },
+    { path: '/owner/toys/create', name: 'OwnerToysCreate', component: OwnerToysCreate,  meta: { layout: 'owner' }, },
+    { path: '/owner/toys/:toy', name: 'OwnerToysShow', component: OwnerToysShow, props: true,  meta: { layout: 'owner' }, },
+    { path: '/owner/toys/:toy/edit', name: 'OwnerToysEdit', component: OwnerToysEdit, props: true,  meta: { layout: 'owner' }, },
 
     // 管理者:在庫画面ルート
-    { path: '/owner/stocks', name: 'OwnerStocksIndex', component: OwnerStocksIndex },
-    { path: '/owner/stocks/:stock', name: 'OwnerStockIncrement', component: OwnerStockIncrement, props: true },
-    { path: '/owner/stocks/:stock/update', name: 'OwnerStocksUpdate', component: OwnerStocksUpdate, props: true },
-    { path: '/owner/stocks/history', name: 'OwnerHistoryIndex', component: OwnerHistoryIndex },
+    { path: '/owner/stocks', name: 'OwnerStocksIndex', component: OwnerStocksIndex,  meta: { layout: 'owner' }, },
+    { path: '/owner/stocks/:stock', name: 'OwnerStockIncrement', component: OwnerStockIncrement, props: true,  meta: { layout: 'owner' }, },
+    { path: '/owner/stocks/:stock/update', name: 'OwnerStocksUpdate', component: OwnerStocksUpdate, props: true,  meta: { layout: 'owner' }, },
+    { path: '/owner/stocks/history', name: 'OwnerHistoryIndex', component: OwnerHistoryIndex,  meta: { layout: 'owner' }, },
 
     // 管理者:予約画面ルート
-    { path: '/owner/reserve', name: 'OwnerReserveCreate', component: OwnerReserveCreate },
+    { path: '/owner/reserve', name: 'OwnerReserveCreate', component: OwnerReserveCreate,  meta: { layout: 'owner' }, },
 
     // 管理者:カテゴリー画面ルート
-    { path: '/owner/categories', name: 'OwnerCategoryIndex', component: OwnerCategoryIndex },
-    { path: '/owner/categories/create', name: 'OwnerCategoryCreate', component: OwnerCategoryCreate },
-    { path: '/owner/categories/:category/edit', name: 'OwnerCategoryEdit', component: OwnerCategoryEdit, props: true },
+    { path: '/owner/categories', name: 'OwnerCategoryIndex', component: OwnerCategoryIndex,  meta: { layout: 'owner' }, },
+    { path: '/owner/categories/create', name: 'OwnerCategoryCreate', component: OwnerCategoryCreate,  meta: { layout: 'owner' }, },
+    { path: '/owner/categories/:category/edit', name: 'OwnerCategoryEdit', component: OwnerCategoryEdit, props: true,  meta: { layout: 'owner' }, },
 
     // 管理者:シリーズ画面ルート
-    { path: '/owner/series', name: 'OwnerSeriesIndex', component: OwnerSeriesIndex },
-    { path: '/owner/series/create', name: 'OwnerSeriesCreate', component: OwnerSeriesCreate },
-    { path: '/owner/series/:series/edit', name: 'OwnerSeriesEdit', component: OwnerSeriesEdit, props: true },
+    { path: '/owner/series', name: 'OwnerSeriesIndex', component: OwnerSeriesIndex,  meta: { layout: 'owner' }, },
+    { path: '/owner/series/create', name: 'OwnerSeriesCreate', component: OwnerSeriesCreate,  meta: { layout: 'owner' }, },
+    { path: '/owner/series/:series/edit', name: 'OwnerSeriesEdit', component: OwnerSeriesEdit, props: true,  meta: { layout: 'owner' }, },
 
     // 管理者:ユーザ管理画面ルート
-    { path: '/owner/users', name: 'OwnerUsersIndex', component: OwnerUsersIndex },
-    { path: '/owner/users/:user', name: 'OwnerUserEdit', component: OwnerUserEdit, props: true },
+    { path: '/owner/users', name: 'OwnerUsersIndex', component: OwnerUsersIndex,  meta: { layout: 'owner' }, },
+    { path: '/owner/users/:user', name: 'OwnerUserEdit', component: OwnerUserEdit, props: true,  meta: { layout: 'owner' }, },
 
     // 管理者:売上分析画面ルート
-    { path: '/owner/sales-analysis', name: 'OwnerSalesAnalysis', component: OwnerSalesAnalysis },
+    { path: '/owner/sales-analysis', name: 'OwnerSalesAnalysis', component: OwnerSalesAnalysis,  meta: { layout: 'owner' }, },
 
     //Toyzamasディレクトリのルート設定
-    { path: '/toyzamas/:id/top', name:'ToyzamasTop', component: ToyzamasTop },
+    { path: '/toyzamas/:id/top', name:'ToyzamasTop', component: ToyzamasTop,  meta: { layout: 'user' }, },
     // ユーザ:商品画面
-    { path: '/toyzamas/toys', name: 'UserToysIndex', component: UserToysIndex, props: true },
-    { path: '/toyzamas/toys/:toy', name: 'UserToysShow', component: UserToysShow, props: true },
+    { path: '/toyzamas/toys', name: 'UserToysIndex', component: UserToysIndex, props: true,  meta: { layout: 'user' },  },
+    { path: '/toyzamas/toys/:toy', name: 'UserToysShow', component: UserToysShow, props: true,  meta: { layout: 'user' },  },
 
     // ユーザ:カート画面
-    { path: '/toyzamas/cart', name: 'UserCartIndex', component: UserCartIndex, props: true },
+    { path: '/toyzamas/cart', name: 'UserCartIndex', component: UserCartIndex, props: true,  meta: { layout: 'user' },  },
     
     // ユーザ:購入完了画面
-    { path: '/toyzamas/buy/completion', name: 'UserBuyCompletion', component: UserBuyCompletion, props: true },
+    { path: '/toyzamas/buy/completion', name: 'UserBuyCompletion', component: UserBuyCompletion, props: true,  meta: { layout: 'user' },  },
 
     // ユーザ:購入履歴画面
-    { path: '/toyzamas/buy/history', name: 'UserHistoryIndex', component: UserHistoryIndex, props: true },
+    { path: '/toyzamas/buy/history', name: 'UserHistoryIndex', component: UserHistoryIndex, props: true,  meta: { layout: 'user' },  },
 
     // ユーザ:予約画面
-    { path: '/toyzamas/reserves', name: 'UserReservesIndex', component: UserReservesIndex, props: true },
+    { path: '/toyzamas/reserves', name: 'UserReservesIndex', component: UserReservesIndex, props: true,  meta: { layout: 'user' },  },
 
     // ユーザ:お気に入り画面
-    { path: '/toyzamas/favorites', name: 'UserFavoritesIndex', component: UserFavoritesIndex, props: true },
+    { path: '/toyzamas/favorites', name: 'UserFavoritesIndex', component: UserFavoritesIndex, props: true,  meta: { layout: 'user' },  },
   ],
 })
 
