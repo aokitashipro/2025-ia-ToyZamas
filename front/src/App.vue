9b20c8cf6eb18561696ef902dc7ea4d509eb7d65
shindo mitsuki <m.shindo@core-tech.jp>
2025-06-25 16:28:02 +0900

    進藤がApp.vueを レイアウトを切り替えできるように修正

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <Header v-if="isAuthenticated" />
-    <main class="container">
-  
-      <div v-if="!isAuthenticated">
-        <h2>といザます</h2>
-        <RouterLink to="/login">機能を使うにはログインが必要です。</RouterLink>
-      </div>
-          <router-view /> <!-- ページごとに切り替わる部分 -->
-    </main>
-    <Footer v-if="isAuthenticated" />
-  </div>
+      <component :is="layoutComponent">
+        <router-view /> <!-- ページごとに切り替わる部分 -->
+      </component>
 </template>
 
 <script setup>
-import Header from './components/OwnerHeader.vue';
-import Footer from './components/OwnerFooter.vue';
-// import { computed } from 'vue';
-// import { useAuthStore } from './stores/auth';//Piniaで定義した「認証情報管理ストア」を呼び出す
-// const authStore = useAuthStore();
-// const isAuthenticated = computed(() => authStore.isLoggedIn);
-// 現在ログイン中かどうか（トークンがあるか）を返すgetter
+// import Header from './components/Header.vue';
+// import Footer from './components/Footer.vue';
+import OwnerLayout from '@/layouts/OwnerLayout.vue'
+import UserLayout from '@/layouts/UserLayout.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+
+// ルートのmeta情報に応じてレイアウトを切り替え
+const layoutComponent = computed(() => {
+  const layout = route.meta.layout
+  if (layout === 'owner') return OwnerLayout
+  return UserLayout
+})
 </script>
 
 <style scoped>
