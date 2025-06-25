6f6b667e6f54920d4c76d3b7f196a90607c0ed4e
shindo mitsuki <m.shindo@core-tech.jp>
2025-06-25 12:39:56 +0900

    進藤がApp.vueを一部変更

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,86 +1,30 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <Header v-if="isAuthenticated" />
+    <main class="container">
+  
+      <div v-if="!isAuthenticated">
+        <h2>といザます</h2>
+        <RouterLink to="/login">機能を使うにはログインが必要です。</RouterLink>
+      </div>
+          <router-view /> <!-- ページごとに切り替わる部分 -->
+    </main>
+    <Footer v-if="isAuthenticated" />
+  </div>
 </template>
 
+<script setup>
+import Header from './components/OwnerHeader.vue';
+import Footer from './components/OwnerFooter.vue';
+// import { computed } from 'vue';
+// import { useAuthStore } from './stores/auth';//Piniaで定義した「認証情報管理ストア」を呼び出す
+// const authStore = useAuthStore();
+// const isAuthenticated = computed(() => authStore.isLoggedIn);
+// 現在ログイン中かどうか（トークンがあるか）を返すgetter
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  padding: 20px;
 }
 </style>
